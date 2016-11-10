8271ba4860fa0b0b9d774e05381cc742b98c8a1b
Todd <toddthetoad95@gmail.com>
2016-11-10 16:49:32 -0600

    initialized audiorecord successfully, asked for permissions

--- app/src/main/java/com/example/todd/knock/MainActivity.java
+++ app/src/main/java/com/example/todd/knock/MainActivity.java
@@ -7,9 +7,14 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.support.annotation.IntegerRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView mGyro_y;
     private TextView mGyro_z;
 
+    private AudioRecord mAudioRecorder;
 
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -34,6 +40,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
+    private static final int RECORD_REQUEST_CODE = 101;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +69,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             );
         }
 
+        permission = ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                RECORD_REQUEST_CODE);
+
+
         mAccel_x = (TextView) findViewById(R.id.textView);
         mAccel_y = (TextView) findViewById(R.id.textView2);
         mAccel_z = (TextView) findViewById(R.id.textView3);
@@ -69,8 +83,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mGyro_y = (TextView) findViewById(R.id.textView5);
         mGyro_z = (TextView) findViewById(R.id.textView6);
 
-
-
+        int freq = 44100;
+        int chan = AudioFormat.CHANNEL_IN_STEREO;
+        int enc  = AudioFormat.ENCODING_PCM_16BIT;
+        int src  = MediaRecorder.AudioSource.DEFAULT;
+        int buflen = AudioRecord.getMinBufferSize(freq, chan, enc);
+        mAudioRecorder = new AudioRecord(src,freq,chan,enc,20*buflen);
+        if(mAudioRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
+            Log.d("AUDIO", "AudioRecord Initialized");
+        } else {
+            Log.d("AUDIO", "AudioRecord failed to initialize");
+        }
 
     }
 
